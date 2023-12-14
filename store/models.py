6d2598e2dee66cb1a2b79688bbcd09b81323476b
oscorchan <135039783+oscorchan@users.noreply.github.com>
2023-12-14 19:58:44 +0100

    Ajout des premiers modèles

--- store/models.py
+++ store/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+'''
+Produits:
+
+-Nom
+-Prix
+-Quantité en stock
+-Matériaux
+-Catégorie
+-Description
+-Image
+'''
+
+class Material(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_lenght=50)
+    price = models.DecimalField(default=0)
+    quantity = models.PositiveIntegerField(default=0)
+    materials = models.ManyToManyField(Material)
+    category = models.CharField(max_lenght=50)
+    description = models.TextField(blank=True)
+    thumbnail = models.ImageField(upload_to='products', blank=True, null=True)
+    
