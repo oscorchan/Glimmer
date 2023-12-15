d7198c37b9ff19d3bfac42e472c122f1a3a91130
oscorchan <135039783+oscorchan@users.noreply.github.com>
2023-12-15 14:59:13 +0100

    ajout formulaire inscription

--- shop/urls.py
+++ shop/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path
 from store.views import index, product_detail
 from django.conf.urls.static import static
+from accounts.views import signup
 
 from shop import settings
 
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
-    path('product/<str:slug>/', product_detail, name='product')
+    path('product/<str:slug>/', product_detail, name='product'),
+    path('signup', signup, name='signup')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
