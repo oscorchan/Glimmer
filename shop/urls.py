e96bca1889954bb9825e71788f246b45a580e608
oscorchan <135039783+oscorchan@users.noreply.github.com>
2023-12-15 17:01:48 +0100

    debut de la gestion du panier

--- shop/urls.py
+++ shop/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from store.views import index, product_detail
+from store.views import index, product_detail, add_to_cart
 from django.conf.urls.static import static
 from accounts.views import signup, login, logout
 
@@ -26,6 +26,7 @@ urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('product/<str:slug>/', product_detail, name='product'),
+    path('product/<str:slug>/add-to-cart/', add_to_cart, name='add-to-cart'),
     path('signup/', signup, name='signup'),
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from store.models import Cart
 
 from store.models import Product
 
@@ -11,3 +11,8 @@ def index(request):
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug)
     return render(request, 'store/product.html', context={"product": product})
+
+def add_to_cart(request, slug):
+    user = request.user
+    product = get_object_or_404(Product, slug=slug)
+    Cart.objects.get(user=user)
