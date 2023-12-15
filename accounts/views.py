665be48abc724dec1a2448ebe7bd5de6254519f1
oscorchan <135039783+oscorchan@users.noreply.github.com>
2023-12-15 15:36:24 +0100

    gestion signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model, login as auth_login
+from django.contrib import messages
+
+User = get_user_model()
 
 def signup(request):
+    if request.method =='POST':
+        username = request.POST.get("username")
+        firstname = request.POST.get("firstname")
+        lastname = request.POST.get("lastname")
+        password = request.POST.get("password")
+        
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Un utilisateur avec ce nom d'utilisateur existe déjà.")
+            return render(request, 'accounts/signup.html')
+        
+        user = User.objects.create_user(username=username, 
+                                        first_name=firstname, 
+                                        last_name=lastname, 
+                                        password=password)
+        
+        auth_login(request, user)
+        return redirect('index')
+    
     return render(request, 'accounts/signup.html')
 
 def login(request):
+    if request.method =='POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+    
     return render(request, 'accounts/login.html')
