ebd6a944241e554349602967e2f935acdcfe5c95
oscorchan <135039783+oscorchan@users.noreply.github.com>
2023-12-15 15:12:07 +0100

    creation page connexion

--- accounts/views.py
+++ accounts/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def signup(request):
     return render(request, 'accounts/signup.html')
+
+def login(request):
+    return render(request, 'accounts/login.html')
--- shop/urls.py
+++ shop/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from store.views import index, product_detail
 from django.conf.urls.static import static
-from accounts.views import signup
+from accounts.views import signup, login
 
 from shop import settings
 
@@ -26,5 +26,6 @@ urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('product/<str:slug>/', product_detail, name='product'),
-    path('signup', signup, name='signup')
+    path('signup', signup, name='signup'),
+    path('login', login, name='login'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
